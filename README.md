65cf1cde58dcf37e0426d67e5a45eae8798d3683
Stelios G. Sfakianakis <sgsfak@gmail.com>
2020-01-09 00:35:01 +0200

    update docs

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 A Clojure library for accessing the [OpenCage Geocoding API](https://opencagedata.com/)
 
-[![Clojars Project](https://img.shields.io/clojars/v/clj-ocgeo.svg)](https://clojars.org/clj-ocgeo)
+[![Clojars Project](https://img.shields.io/clojars/v/clj-ocgeo.svg)](https://clojars.org/clj-ocgeo) [![cljdoc badge](https://cljdoc.org/badge/clj-ocgeo/clj-ocgeo)](https://cljdoc.org/badge/clj-ocgeo/clj-ocgeo/CURRENT)
+
 
 ## Getting started
 
@@ -63,5 +64,93 @@ A number of keyword arguments corresponding to the [optional parameters](https:/
 		:countrycode "de" :no_annotations true :min_confidence 5)
 ```
 
+Another example, showing the first result of a request is shown below:
 
+```clojure
+(when-let [r (-> (forward-request "Friedrich-Ebert-Straße 7" "6d0e711d72d74daeb2b0bfd2a5cdfdba")
+                 :results
+                 first)]
+  (pprint r))
+
+;; {:annotations
+;;  {:timezone
+;;   {:name "Europe/Berlin",
+;;    :now_in_dst 0,
+;;    :offset_sec 3600,
+;;    :offset_string "+0100",
+;;    :short_name "CET"},
+;;   :roadinfo
+;;   {:drive_on "right",
+;;    :road "Friedrich-Ebert-Straße",
+;;    :speed_in "km/h"},
+;;   :geohash "u1jrt9ty1t8rg3r5wttm",
+;;   :what3words {:words "episodes.mass.below"},
+;;   :flag "🇩🇪",
+;;   :OSM
+;;   {:edit_url
+;;    "https://www.openstreetmap.org/edit?way=125838041#map=17/51.95266/7.63247",
+;;    :note_url
+;;    "https://www.openstreetmap.org/note/new#map=17/51.95266/7.63247&layers=N",
+;;    :url
+;;    "https://www.openstreetmap.org/?mlat=51.95266&mlon=7.63247#map=17/51.95266/7.63247"},
+;;   :currency
+;;   {:symbol "€",
+;;    :name "Euro",
+;;    :thousands_separator ".",
+;;    :iso_numeric "978",
+;;    :subunit_to_unit 100,
+;;    :iso_code "EUR",
+;;    :decimal_mark ",",
+;;    :alternate_symbols [],
+;;    :symbol_first 1,
+;;    :smallest_denomination 1,
+;;    :subunit "Cent",
+;;    :html_entity "&#x20AC;"},
+;;   :MGRS "32UMC0602156656",
+;;   :Mercator {:x 849643.007, :y 6757899.137},
+;;   :qibla 128.55,
+;;   :sun
+;;   {:rise
+;;    {:apparent 1578468960,
+;;     :astronomical 1578461580,
+;;     :civil 1578466560,
+;;     :nautical 1578463980},
+;;    :set
+;;    {:apparent 1578497760,
+;;     :astronomical 1578505140,
+;;     :civil 1578500160,
+;;     :nautical 1578502740}},
+;;   :DMS {:lat "51° 57' 9.57564'' N", :lng "7° 37' 56.90280'' E"},
+;;   :callingcode 49,
+;;   :Maidenhead "JO31tw58vp",
+;;   :UN_M49
+;;   {:regions
+;;    {:DE "276", :EUROPE "150", :WESTERN_EUROPE "155", :WORLD "001"},
+;;    :statistical_groupings ["MEDC"]}},
+;;  :bounds
+;;  {:northeast {:lat 51.9528202, :lng 7.6325938},
+;;   :southwest {:lat 51.9525445, :lng 7.6323594}},
+;;  :components
+;;  {:city_district "Münster-Mitte",
+;;   :suburb "Innenstadtring",
+;;   :continent "Europe",
+;;   :political_union "European Union",
+;;   :neighbourhood "Josef",
+;;   :house_number "7",
+;;   :city "Münster",
+;;   :_type "building",
+;;   :county "Münster",
+;;   :postcode "48153",
+;;   :state "North Rhine-Westphalia",
+;;   :ISO_3166-1_alpha-3 "DEU",
+;;   :country_code "de",
+;;   :state_district "Regierungsbezirk Münster",
+;;   :ISO_3166-1_alpha-2 "DE",
+;;   :country "Germany",
+;;   :road "Friedrich-Ebert-Straße"},
+;;  :confidence 10,
+;;  :formatted "Friedrich-Ebert-Straße 7, 48153 Münster, Germany",
+;;  :geometry {:lat 51.9526599, :lng 7.632473}}
+nil
+```
 
--- src/clj_ocgeo/core.clj
+++ src/clj_ocgeo/core.clj
@@ -1,7 +1,8 @@
 (ns clj-ocgeo.core
-  (:require [clojure.data.json :as json])
-  (:require [clojure.string :as string])
-  (:require [clj-http.client :as client]))
+  (:require
+    [clojure.data.json :as json]
+    [clojure.string :as string]
+    [clj-http.client :as client]))
 
 (defn- build-qs [options]
   (let [truthy? #(and % (not= 0 %) (not (empty? %)))
@@ -22,7 +23,37 @@
 
 (defn forward-request
   "Makes forward geocoding request which given an address or
-   place name returns its coordinates and optional annotations."
+   place name returns its coordinates and optional annotations.
+   
+   The query `q` should be a description of a place or an address, see
+   the [detailed guide](https://github.com/OpenCageData/opencagedata-misc-docs/blob/master/query-formatting.md)
+   on how to format the query.
+   `api-key` is the API key given during the registration with the API. 
+   A number of optional keyword arguments can also be supplied:
+
+    * `:abbrv`: If true, attempt to abbreviate and shorten the formatted string we return.
+    * `:add_request`: If true, the various request parameters are added to the response for ease of debugging.
+    * `:bounds`: This value will restrict the possible results to a defined bounding box.
+       Its value hould be specified as a vector with two coordinate points forming ther
+       south-west and north-east corners of a bounding box. For example:
+       `[-0.563160,51.280430,0.278970,51.683979]` (min lon, min lat, max lon, max lat).
+    * `:countrycode`: A string that restricts results to the specified country/territory or countries.
+       The country code is a two letter code as defined by the ISO 3166-1 Alpha 2 standard.
+       E.g. `gb` for the United Kingdom, `fr` for France, `us` for United States.
+    * `:language`: An IETF format language code (such as `es` for Spanish or `pt-BR` for Brazilian Portuguese),r
+       or `native` in which case we will attempt to return the response in the local language(s).
+    * `:limit`: The maximum number of results we should return. Default is 10. Maximum allowable value is 100.
+    * `:min_confidence`: An integer from 1-10. Only results with at least this confidence will be returned.
+    * `:no_annotations`: When true results will not contain annotations.
+    * `:no_dedupe`: When true results will not be deduplicated.
+    * `:no_record`: When true the query contents are not logged.
+    * `:proximity`: Provides the geocoder with a hint to bias results in favour of those
+       closer to the specified location.
+       The value is a vector of two elements representing a point with latitude,
+       longitude coordinates in decimal format. For example: `[51.952659, 7.632473]`
+    * `:roadinfo`:  When true the behaviour of the geocoder is changed to
+       attempt to match the nearest road (as opposed to address).
+       If possible we also fill additional information in the roadinfo annotation."
   [query api-key & {:keys [abbrev bounds countrycode
                            language limit min_confidence
                            no_annotations no_dedupe
@@ -42,7 +73,9 @@
 (defn reverse-request
   "Make a reverse request, i.e. given a latitude and a longitude
    coordinates it returns a list of human understandable place names
-   or addresses"
+   or addresses.
+   
+   See [[forward-request]] for the options supported"
   [latitude longitude api-key & opts]
   (apply forward-request (string/join "," [latitude, longitude]) api-key opts))
 
